eb335b2ef0aee08069f681206395e181444d149e
Absolom <Absmugz09@.yahoo.com>
2013-09-01 19:59:28 +0200

    loading twitter bootstrap css  framework
    loading twitter bootstrap css and javascript framework

--- application/libraries/Admin_Controller.php
+++ application/libraries/Admin_Controller.php
@@ -5,5 +5,6 @@ class Admin_Controller extends MY_Controller
 	function __construct ()
 	{
 		parent::__construct();
+                $this->data['meta_title'] = 'Info-age business solutions';
 	}
 }
---
+++ application/views/admin/_layout_main.php
@@ -0,0 +1,6 @@
+<?php $this->load->view('admin/components/page_head'); ?>
+  <body>
+    <h1>Hello, world!</h1>
+    
+<?php $this->load->view('admin/components/page_tail'); ?>
+
